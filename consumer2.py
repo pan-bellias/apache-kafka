703124f158a71ff34b0473f11c9f0900af10078f
pan-bellias <belliaspan@quintessential.gr>
2022-08-01 14:30:42 +0300

    temperature use case started

--- consumer2.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-import env as e
-
-# join a consumer group for dynamic partition assignment and offset commits
-consumer = KafkaConsumer(e.topic, group_id=e.group)
-for msg in consumer:
-    print (msg)
--- consumer3.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer, TopicPartition
-import env as e
-
-# manually assign the partition list for the consumer
-consumer = KafkaConsumer(e.topic, bootstrap_servers=[e.bootstrap_servers])
-consumer.assign([TopicPartition('foobar', 2)])
-msg = next(consumer)
